f1ce9caa5f7ee9df4669d116df60a6d86ddce3d6
shiori0221 <sarasa8221@gmail.com>
2021-07-26 11:37:39 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,14 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname,       presence: true
   validates :password,:password_confirmation,format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-  validates :lastname,       presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :firstname,      presence: true, format: {with: /\A[ぁ-んァ-ン一-龥]/}
-  validates :lastname_kana,  presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :firstname_kana, presence: true, format: {with: /\A[ァ-ヶー－]+\z/}
-  validates :birthday,       presence: true
+  with_options presence: true do
+    validates :nickname
+    validates :lastname,       format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :firstname,      format: {with: /\A[ぁ-んァ-ン一-龥]/}
+    validates :lastname_kana,  format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :firstname_kana, format: {with: /\A[ァ-ヶー－]+\z/}
+    validates :birthday
+  end
 end
