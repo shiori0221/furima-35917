98df706488e0be51234325a068013efae0dea29c
shiori0221 <sarasa8221@gmail.com>
2021-07-27 16:06:30 +0900

    アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,17 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :delivery_charge
+  belongs_to :shipping_address
+  belongs_to :days_to_delivery
+
   with_options presence: true do
     validates :product
     validates :price
     validates :description
+  end
+  with_options numericality: { other_than: 1, message: "can't be blank"} do
     validates :status_id
     validates :delivery_charge_id
     validates :shipping_address_id
