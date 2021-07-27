58a51cffc61d9413db020c3d6872dc87b6f5af92
shiori0221 <sarasa8221@gmail.com>
2021-07-27 11:37:37 +0900

    ルーティングとコントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,17 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:product, :price, :description, :status_id, :delivery_charge_id, :shippoing_address_id, :days_to_delivery_id, :category_id)
+  end
 end
