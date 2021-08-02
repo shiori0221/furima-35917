001bf34414589cd64ca70cd4cb4507f382eb60fa
shiori0221 <sarasa8221@gmail.com>
2021-08-02 18:34:06 +0900

    コントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :move_to_index, only: [:edit, :update]
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path
     else
@@ -43,7 +41,11 @@ class ItemsController < ApplicationController
                                  :days_to_delivery_id, :category_id, :image).merge(user_id: current_user.id)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def move_to_index
-    redirect_to root_path unless current_user.id == Item.find(params[:id]).user_id
+    redirect_to root_path unless current_user.id == @item.user_id
   end
 end
