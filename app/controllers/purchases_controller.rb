6b6a61d5457c24a3f1f2d62f5217052d1d0707a7
shiori0221 <sarasa8221@gmail.com>
2021-08-07 15:53:56 +0900

    クレジット決済機能の実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -9,6 +9,7 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_shopping_address = PurchaseShoppingAddress.new(purchase_params)
     if @purchase_shopping_address.valid?
+      pay_item
       @purchase_shopping_address.save
       redirect_to  root_path
     else
@@ -19,8 +20,16 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_shopping_address).permit(:postal_code, :shipping_address_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:purchase_shopping_address).permit(:postal_code, :shipping_address_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
+  def pay_item
+    Payjp::api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,
+      card: purchase_params[:token],
+      currency: 'jpy'
+    )
+  end
 
 end
--- app/models/purchase_shopping_address.rb
+++ app/models/purchase_shopping_address.rb
@@ -1,6 +1,6 @@
 class PurchaseShoppingAddress
   include ActiveModel::Model
-  attr_accessor :postal_code, :shipping_address_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :postal_code, :shipping_address_id, :municipality, :address, :building_name, :phone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -10,6 +10,7 @@ class PurchaseShoppingAddress
     validates :phone_number, format: {with: /\A[0-9]+\z/ }, length: { maximum: 11 },numericality: true
     validates :user_id
     validates :item_id
+    validates :token
   end
 
   def save
