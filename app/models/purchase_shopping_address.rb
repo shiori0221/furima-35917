3d4e3a6917a030be2f541e95a9d05675b35acaeb
shiori0221 <sarasa8221@gmail.com>
2021-08-08 10:58:54 +0900

    モデル単体テストコード

--- app/models/purchase_shopping_address.rb
+++ app/models/purchase_shopping_address.rb
@@ -4,10 +4,10 @@ class PurchaseShoppingAddress
 
   with_options presence: true do
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-    validates :shipping_address_id, numericality: {other_than: 0, message: "can't be blank"}
+    validates :shipping_address_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :municipality
     validates :address
-    validates :phone_number, format: {with: /\A[0-9]+\z/ }, length: { minimum: 10, maximum: 11 },numericality: true
+    validates :phone_number, format: {with: /\A[0-9]+\z/ }, length: { minimum: 10, maximum: 11 }
     validates :user_id
     validates :item_id
     validates :token
