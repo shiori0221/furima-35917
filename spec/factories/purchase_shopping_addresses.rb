2ab9bf8b87b7614b085d835008e6bea816d0b0a3
shiori0221 <sarasa8221@gmail.com>
2021-08-09 11:49:39 +0900

    テストコードの修正

--- spec/factories/purchase_shopping_addresses.rb
+++ spec/factories/purchase_shopping_addresses.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
     postal_code { '123-4567' }
     shipping_address_id { 2 }
     municipality { '横浜市' }
-    address { 1 - 1 }
+    address { "1 - 1" }
     building_name { '東京ハイツ' }
     phone_number { '09012345678' }
   end
