5250f7de7e9d853dbc29a3ac84ad7c1eea96e816
shiori0221 <sarasa8221@gmail.com>
2021-08-25 11:01:28 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show, :search]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :search]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
   before_action :redirect_root, only: [:edit, :update]
 
   def index
