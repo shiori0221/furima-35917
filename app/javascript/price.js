de09b1b3545e85a4d13f15eea390e2096d1c8bff
shiori0221 <sarasa8221@gmail.com>
2021-08-29 21:05:14 +0900

    条件分岐の追加

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,13 +1,15 @@
 function price (){
-  const itemPrice = document.getElementById("item-price");
-  itemPrice.addEventListener("keyup", () => {
-    const addTax = itemPrice.value * 0.1;
-    const addTaxPrice = document.getElementById("add-tax-price")
-    addTaxPrice.innerHTML = `${addTax}`;
-    const profit = itemPrice.value - addTax
-    const profitPrice = document.getElementById("profit")
-    profitPrice.innerHTML = `${profit}`;
-  })
-}
+  if ( document.getElementById("item-price")){
+    const itemPrice = document.getElementById("item-price");
+    itemPrice.addEventListener("keyup", () => {
+      const addTax = itemPrice.value * 0.1;
+      const addTaxPrice = document.getElementById("add-tax-price");
+      addTaxPrice.innerHTML = `${addTax}`;
+      const profit = itemPrice.value - addTax;
+      const profitPrice = document.getElementById("profit");
+      profitPrice.innerHTML = `${profit}`;
+    });
+  }
+};
 
 window.addEventListener('load', price)
